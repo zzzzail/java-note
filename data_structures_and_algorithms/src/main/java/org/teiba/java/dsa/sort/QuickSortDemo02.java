d15b42ccdb457e57c99b048375e3d856222c449d
zail <zzzzail.mail@gmail.com>
2020-08-05 16:47:44 +0800

    加油

--- data_structures_and_algorithms/src/main/java/org/teiba/java/dsa/sort/QuickSortDemo02.java
+++ data_structures_and_algorithms/src/main/java/org/teiba/java/dsa/sort/QuickSortDemo02.java
@@ -1,9 +1,18 @@
 package org.teiba.java.dsa.sort;
 
 /**
+ * 快速排序
+ *
  * @author zail
  */
 public class QuickSortDemo02 {
+    
+    public static void main(String[] args) {
+        int N = 10_000_000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, N);
+        SortTestHelper.testSort(QuickSortDemo02.class, "sort", Integer[].class, arr);
+        // System.out.println(Arrays.toString(arr));
+    }
     
     public static void sort(Integer[] arr) {
         int n = arr.length;
@@ -28,6 +37,8 @@ public class QuickSortDemo02 {
     
     /**
      * 快速排序核心方法：分区
+     * 对arr[l...r]部分进行partition操作
+     * 返回p，使得arr[l...p-1] < arr[p]; arr[p+1...r] > arr[p]
      *
      * @param arr 数组
      * @param l   左指针
@@ -36,9 +47,26 @@ public class QuickSortDemo02 {
      */
     public static int partition(Integer[] arr, int l, int r) {
         // 选择第一个元素作为分区点
-        Integer p = arr[0];
+        Integer v = arr[l];
         
-        return 0;
+        // 从l+1开始逐步遍历整个数组，arr[l+1...j] < v; arr[j+1...i] > v
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (arr[i] < v) {
+                swap(arr, j + 1, i);
+                j++;
+            }
+        }
+        
+        // 把l位置的元素和j位置的元素进行交换
+        swap(arr, l, j);
+        return j;
+    }
+    
+    public static void swap(Integer[] arr, int i1, int i2) {
+        Integer t = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = t;
     }
     
 }
