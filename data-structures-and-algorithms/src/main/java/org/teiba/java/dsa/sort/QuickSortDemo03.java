ee7f0ce761056bf0f45841e12309b10700dbaab3
zail <zzzzail.mail@gmail.com>
2020-08-06 13:06:52 +0800

    优化2：双路排序

--- data-structures-and-algorithms/src/main/java/org/teiba/java/dsa/sort/QuickSortDemo03.java
+++ data-structures-and-algorithms/src/main/java/org/teiba/java/dsa/sort/QuickSortDemo03.java
@@ -1,5 +1,7 @@
 package org.teiba.java.dsa.sort;
 
+import java.util.Arrays;
+
 /**
  * @author zail
  */
@@ -9,7 +11,7 @@ public class QuickSortDemo03 {
         int N = 10_000_000;
         Integer[] arr = SortTestHelper.generateRandomArray(N, 0, N);
         SortTestHelper.testSort(QuickSortDemo03.class, "sort", Integer[].class, arr);
-        // System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
     }
     
     public static void sort(Integer[] arr) {
@@ -24,11 +26,18 @@ public class QuickSortDemo03 {
      * @param r   右指针
      */
     private static void sortRecursion(Integer[] arr, int l, int r) {
+        // 优化2：如果需要排序数组中的元素个数小于等于15的时候，把排序算法退化成插入排序
+        if (r - l <= 15) {
+            InsertionSortDemo04.sort(arr, l, r);
+            return;
+        }
+        
         // 递归的结束条件
         if (l >= r) return;
         
         // 对数组进行partition操作
-        int p = partition(arr, l, r);
+        // int p = partition(arr, l, r);
+        int p = partition2(arr, l, r);
         sortRecursion(arr, l, p - 1);
         sortRecursion(arr, p + 1, r);
     }
@@ -44,6 +53,7 @@ public class QuickSortDemo03 {
     private static int partition(Integer[] arr, int l, int r) {
         // 优化1：设置真随机种子
         swap(arr, l, (int) (Math.random() * (r - l + 1) + l));
+        
         // 把数组第一个元素作为分区点
         Integer v = arr[l];
         
@@ -57,6 +67,26 @@ public class QuickSortDemo03 {
         }
         
         // 最后把v位置的元素和j位置的元素进行交换
+        swap(arr, l, j);
+        return j;
+    }
+    
+    private static int partition2(Integer[] arr, int l, int r) {
+        // 设置真随机种子
+        swap(arr, l, (int) (Math.random() * (r - l + 1) + l));
+        // 把数组第一个元素作为分区点
+        Integer v = arr[l];
+        
+        // arr[l+1...i) <= v; arr(j...r] >= v
+        int i = l + 1, j = r;
+        while (true) {
+            while (i <= r && arr[i] <= v) i++;
+            while (j >= l + 1 && arr[j] >= v) j--;
+            if (i > j) break;
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
         swap(arr, l, j);
         return j;
     }
