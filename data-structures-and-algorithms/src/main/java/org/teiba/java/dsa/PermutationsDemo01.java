f1812d5896ceb739a9b9860c6fe9e99762707c7f
zail <zzzzail.mail@gmail.com>
2020-08-14 16:29:45 +0800

    全排列的东西还没有搞懂

---
+++ data-structures-and-algorithms/src/main/java/org/teiba/java/dsa/PermutationsDemo01.java
@@ -0,0 +1,51 @@
+package org.teiba.java.dsa;
+
+/**
+ * 全排列问题
+ *
+ * @author zail
+ */
+public class PermutationsDemo01 {
+    
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4};
+        printPermutations(arr, arr.length, arr.length);
+    }
+    
+    /**
+     * 对数组中的所有数字，以n个为一组进行全排列
+     * int[] a = {1, 2, 3, 4};
+     * printPermutations(a, 4, 4);
+     *
+     * 假设数组中存储的是1，2， 3...n。
+     * f(1,2,...n) = {最后一位是1, f(n-1)} + {最后一位是2, f(n-1)} +...+{最后一位是n, f(n-1)}。
+     * k = 0就是结束条件
+     *
+     * @param arr 数组
+     * @param n   n个数字
+     * @param k   表示要处理的子数组的数据个数
+     */
+    public static void printPermutations(int[] arr, int n, int k) {
+        // k每次递归就减去1，递归到最后一个数字的时候就打印
+        if (k == 1) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+        
+        for (int i = 0; i < k; i++) {
+            int t = arr[i];
+            arr[i] = arr[k - 1];
+            arr[k - 1] = t;
+            
+            printPermutations(arr, n, k - 1);
+            
+            // 把数组变回去
+            t = arr[i];
+            arr[i] = arr[k - 1];
+            arr[k - 1] = t;
+        }
+    }
+    
+}
